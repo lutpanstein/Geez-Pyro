a6773641ad4dcbe4c11193cf57166e0c4c987e5b
vckyou <79180058+vckyou@users.noreply.github.com>
2023-10-07 02:25:37 +0700

    Update stats.py

--- Geez/modules/basic/stats.py
+++ Geez/modules/basic/stats.py
@@ -14,29 +14,16 @@ YANG NYOLONG REPO INI TRUS DIJUAL JADI PREM, LU GAY...
 """
 
 from datetime import datetime
-from pyrogram import Client, enums, errors
+from pyrogram import Client, enums
 from pyrogram.errors import ChatAdminRequired
 from pyrogram.types import Message
 from geezlibs.geez import geez
 from Geez.modules.basic import add_command_help
-from Geez.modules.basic.profile import extract_user
 from Geez import cmds
 
 @geez("stats", cmds)
 async def stats(client: Client, message: Message):
-    target_user = await extract_user(message)  # Mendapatkan username atau user ID dari pesan
-
-    try:
-        target_user = int(target_user)
-    except ValueError:
-        try:
-            target_user = await client.get_users(target_user)
-            target_user = target_user.id
-        except Exception as e:
-            await message.edit_text(f"`Gagal mendapatkan informasi akun target. Error: {e}`")
-            return
-
-    Man = await message.edit_text(f"`Mengambil info akun {target_user} ...`")
+    Man = await message.edit_text("`Mengambil info akun ...`")
     start = datetime.now()
     u = 0
     g = 0
@@ -44,49 +31,40 @@ async def stats(client: Client, message: Message):
     c = 0
     b = 0
     a_chat = 0
+    Meh = await client.get_me()
 
-    try:
-        target_user = int(target_user)
-        target_user_info = await client.get_chat(target_user)
-    except ValueError:
-        try:
-            user = await client.get_users(target_user)
-        except errors.exceptions.bad_request_400.UsernameNotOccupied:
-            # Tangani kasus ketika pengguna tidak ditemukan
-            target_user_info = target_user
-        except Exception as e:
-            # Tangani kasus kesalahan umum lainnya
-            await message.edit_text(f"Gagal mendapatkan informasi akun target. Error: {e}")
-            return
-
+    # List to store information of groups and supergroups
     group_info = []
 
-    try:
-        async for dialog in client.get_dialogs():  # Tambahkan 'async' sebelum for
-            if dialog.chat.type == enums.ChatType.PRIVATE:
-                u += 1
-            elif dialog.chat.type == enums.ChatType.BOT:
-                b += 1
-            elif dialog.chat.type in (enums.ChatType.GROUP, enums.ChatType.SUPERGROUP):
-                group_info.append((dialog.chat.id, dialog.chat.title))
+    async for dialog in client.get_dialogs():
+        if dialog.chat.type == enums.ChatType.PRIVATE:
+            u += 1
+        elif dialog.chat.type == enums.ChatType.BOT:
+            b += 1
+        elif dialog.chat.type in (enums.ChatType.GROUP, enums.ChatType.SUPERGROUP):
+            
+            group_info.append((dialog.chat.id, dialog.chat.title))
 
-                if dialog.chat.type == enums.ChatType.SUPERGROUP:
-                    sg += 1
-
-                    try:
-                        # ...
-                        except ChatAdminRequired:
-                            pass
-                elif dialog.chat.type == enums.ChatType.CHANNEL:
-                    c += 1
+            if dialog.chat.type == enums.ChatType.SUPERGROUP:
+                sg += 1
+                user_s = await dialog.chat.get_member(int(Meh.id))
+                if user_s.status in (
+                    enums.ChatMemberStatus.OWNER,
+                    enums.ChatMemberStatus.ADMINISTRATOR,
+                ):
+                    a_chat += 1
+        elif dialog.chat.type == enums.ChatType.CHANNEL:
+            c += 1
 
     group_info = group_info[:20]
+
     end = datetime.now()
-    ms = (end - start)
+    ms = (end - start).seconds
+
     group_info_text = "\n".join([f"{id}: {title}" for id, title in group_info])
 
     await Man.edit_text(
-        f"""`Status akun {target_user}, berhasil diambil dalam {ms} detik`
+        f"""`Status akun anda, berhasil diambil dalam {ms} detik`
         ` {u} Pesan Pribadi.`
         `berada di {g} Groups.`
         `berada {sg} Super Groups.`
@@ -96,10 +74,10 @@ async def stats(client: Client, message: Message):
         `Info Grup:\n{group_info_text}`"""
     )
 
+
 add_command_help(
     "stats",
     [
         [f"{cmds}stats", "Mengambil info akun anda."],
-        [f"{cmds}stats @username atau user_id", "Mengambil info akun pengguna lain."],
-    ],
+    ]
 )
