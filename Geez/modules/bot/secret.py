424f8aa86723ca014e005ccaa19984a1d2a2b6b1
vckyou <79180058+vckyou@users.noreply.github.com>
2023-10-14 21:47:03 +0700

    Update secret.py

--- Geez/modules/bot/secret.py
+++ Geez/modules/bot/secret.py
@@ -8,6 +8,7 @@ from base64 import b64encode, b64decode
 from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
+    InputTextMessageContent,
     InlineQueryResultArticle,
 )
 from Geez import app
