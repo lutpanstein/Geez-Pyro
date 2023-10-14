ab0e366110af2037fa3f3301032f6b789dfec8d4
vckyou <79180058+vckyou@users.noreply.github.com>
2023-10-14 20:45:46 +0700

    Update secret.py

--- Geez/modules/bot/secret.py
+++ Geez/modules/bot/secret.py
@@ -12,6 +12,7 @@ from pyrogram.types import (
 from Geez import app
 
 
+# Fungsi untuk mengenkripsi pesan
 def encrypt_message(message, key):
     cipher = AES.new(key, AES.MODE_ECB)
     return b64encode(cipher.encrypt(message))
@@ -24,18 +25,19 @@ def decrypt_message(encrypted_message, key):
 # Inisialisasi kunci acak, pastikan kunci ini disimpan dengan aman
 key = get_random_bytes(16)
 
+# Dictionary untuk menyimpan pesan terenkripsi
+encrypted_messages = {}
 
 # Fungsi yang akan dijalankan ketika bot menerima pesan pribadi
 @app.on_message(filters.private)
 def handle_private_message(client, message: Message):
     try:
-        # Cek apakah ada argumen untuk pesan
-        if len(message.command) < 2:
-            message.reply_text("Cara menggunakan: <pesan> <username/id_pengguna_target>")
+        # Cek apakah pesan dimulai dengan @usernamebot dan memiliki 3 argumen
+        if not message.text.startswith("@usernamebot") or len(message.command) < 3:
             return
 
         # Split pesan untuk mendapatkan teks pesan dan username/id target
-        message_text, target_id = message.text.split(" ", 1)
+        _, message_text, target_id = message.text.split(" ", 2)
         
         # Cek apakah username/id target valid
         if not target_id.startswith("@") and not target_id.isdigit():
@@ -44,15 +46,22 @@ def handle_private_message(client, message: Message):
         # Enkripsi pesan
         encrypted_message = encrypt_message(message_text.encode('utf-8'), key)
 
+        # Simpan pesan terenkripsi
+        encrypted_messages[message.chat.id] = encrypted_message
+
         # Kirim pesan terenkripsi ke pengguna target
         client.send_message(target_id, f"🔒 [Pesan Rahasia] 🔒\n\n{b64encode(encrypted_message).decode('utf-8')}")
 
-        # Kirim tombol untuk dekripsi pesan
-        message.reply_text(f"🔒 Pesan Rahasia terkirim ke {target_id}! 🔒", reply_markup={
-            "inline_keyboard": [
-                [{"text": "Buka Pesan Rahasia", "callback_data": b64encode(encrypted_message).decode('utf-8')}]
-            ]
-        })
+        # Kirim pesan ke chat aktif untuk memungkinkan pengguna membuka pesan
+        client.send_message(
+            chat_id=message.chat.id,
+            text=f"🔒 Pesan Rahasia terkirim ke {target_id}! 🔒",
+            reply_markup={
+                "inline_keyboard": [
+                    [{"text": "Buka Pesan Rahasia", "callback_data": "decrypt"}]
+                ]
+            }
+        )
     except Exception as e:
         message.reply_text(f"Error: {str(e)}")
 
@@ -61,10 +70,12 @@ def handle_private_message(client, message: Message):
 async def button(bot, update):
     try:
         # Dekripsi pesan ketika tombol ditekan
-        encrypted_message = update.data.encode('utf-8')
-        decrypted_message = decrypt_message(encrypted_message, key)
+        encrypted_message = encrypted_messages.get(update.message.chat.id)
+        if encrypted_message:
+            decrypted_message = decrypt_message(encrypted_message, key)
 
-        # Kirim pesan terdekripsi ke pengguna
-        await update.message.reply_text(f"🔓 [Pesan Terbuka] 🔓\n\n{decrypted_message}")
+            # Kirim pesan terdekripsi ke pengguna
+            await update.message.reply_text(f"🔓 [Pesan Terbuka] 🔓\n\n{decrypted_message}")
     except Exception as e:
         await update.message.reply_text(f"Error: {str(e)}")
+
