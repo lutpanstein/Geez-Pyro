dce88fbcaf259abb587444bf30b4d184d56402c5
vckyou <79180058+vckyou@users.noreply.github.com>
2023-10-15 03:11:10 +0700

    Update secret.py

--- Geez/modules/bot/secret.py
+++ Geez/modules/bot/secret.py
@@ -1,27 +1,55 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineQuery, InputTextMessageContent, InlineQueryResultArticle
+import hashlib
 from Geez import app
 
 
-# Fungsi untuk mengirim pesan rahasia
-@app.on_message(filters.command(["start"]))
-def start(client, message):
-    message.reply_text(
-        "Halo! Untuk mengirim pesan rahasia, gunakan format:\n\n"
-        "@usernamebot <pesan> <@username_target>\n\n"
-        "Gunakan tombol di bawah untuk membuka pesan rahasia.",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Buka Pesan Rahasia", callback_data='unlock')]])
-    )
+# Fungsi untuk mengenkripsi pesan
+def encrypt_message(message, username):
+    key = f"{app.get_me().id}:{username}"
+    encrypted_message = hashlib.sha256(key.encode() + message.encode()).hexdigest()
+    return encrypted_message
 
-# Fungsi untuk membuka pesan rahasia
-@app.on_callback_query(filters.regex('^unlock$'))
-def unlock_message(client, callback_query):
-    message = callback_query.message
-    chat_id = message.chat.id
-    text = message.text.split('\n\n')[1]  # Mendapatkan pesan rahasia dari teks pesan
+# Fungsi untuk mendekripsi pesan
+def decrypt_message(encrypted_message, username):
+    key = f"{app.get_me().id}:{username}"
+    decrypted_message = hashlib.sha256(key.encode() + encrypted_message.encode()).hexdigest()
+    return decrypted_message
 
-    # Hanya pengguna dan pengguna target yang dapat melihat pesan rahasia
-    if callback_query.from_user.username == text.split(' ')[1][1:]:
-        app.send_message(chat_id, f"Ini pesan rahasiamu:\n\n{text}")
+# Fungsi untuk mencari pengguna berdasarkan username/id
+def find_user(query):
+    try:
+        user = app.get_users(query)
+        return user
+    except Exception as e:
+        print(e)
+        return None
+
+# Fungsi untuk menangani inline query
+@app.on_inline_query()
+async def inline_query_handler(client, inline_query):
+    input_content = inline_query.query.lower().strip()
+    user = find_user(input_content)
+
+    if user:
+        # Jika pengguna ditemukan, kirim pesan dengan tombol inline
+        message = encrypt_message("Ini adalah pesan rahasia.", user.username)
+        button = InlineKeyboardButton("Buka Pesan", callback_data=f"show_message_{user.id}_{message}")
+        reply_markup = InlineKeyboardMarkup([[button]])
+        result = InlineQueryResultArticle(
+            id=user.id,
+            title=user.username,
+            input_message_content=InputTextMessageContent(message_text="Pesan rahasia", reply_markup=reply_markup)
+        )
+        await inline_query.answer([result], cache_time=0)
     else:
-        app.send_message(chat_id, "Anda tidak memiliki akses untuk membuka pesan rahasia ini.")
+        await inline_query.answer([], cache_time=0)
+
+# Fungsi untuk menanggapi tombol inline
+@app.on_callback_query()
+async def callback_query_handler(client, callback_query):
+    if callback_query.data.startswith("show_message"):
+        _, user_id, message = callback_query.data.split("_")
+        user = await app.get_users(int(user_id))
+        decrypted_message = decrypt_message(message, user.username)
+        await callback_query.answer(decrypted_message)
